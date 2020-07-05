62649470618c5db6b0db2bded5ea9caccbb13be2
renaasou <renanonikkyi@gmail.com>
2020-07-06 07:34:20 +0900

    Updates

---
+++ precache-manifest.778910797f3ac925a79274a4630621d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b7ae6401db8d32a243b4032538487f9",
+    "url": "/mukafan/index.html"
+  },
+  {
+    "revision": "2d1ac9b8e948c70320c3",
+    "url": "/mukafan/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2b7900396bcd34f099e9",
+    "url": "/mukafan/static/js/2.3b0814cb.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/mukafan/static/js/2.3b0814cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d1ac9b8e948c70320c3",
+    "url": "/mukafan/static/js/main.919ee3c1.chunk.js"
+  },
+  {
+    "revision": "8c3a9ead3b27d0a0df66",
+    "url": "/mukafan/static/js/runtime-main.1da7e978.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mukafan/static/media/logo.5d5d9eef.svg"
+  }
+]);
